e0c303f3f9da9fa7f3c04e3087f425cd8ca40e65
KMI1983 <minskkmi@gmail.com>
2022-07-04 19:15:30 +0300

    feat cache

--- cache.go
+++ cache.go
@@ -3,21 +3,58 @@ package cache
 import "time"
 
 type Cache struct {
+	items map[string]Item
+}
+
+type Item struct {
+	Value    string
+	Deadline time.Time
+	Expiring bool
 }
 
 func NewCache() Cache {
-	return Cache{}
+
+	items := make(map[string]Item)
+	return Cache{items: items}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (cache *Cache) Get(key string) (string, bool) {
+	cache.cleanUp()
+	item, ok := cache.items[key]
+	if !ok {
+		return "", false
+	}
+	return item.Value, true
 }
 
-func (receiver) Put(key, value string) {
+func (cache *Cache) Put(key, value string) {
+	cache.items[key] = Item{
+		Value:    value,
+		Expiring: false,
+	}
 }
 
-func (receiver) Keys() []string {
+func (cache *Cache) Keys() []string {
+	cache.cleanUp()
+	var keys []string
+	for k := range cache.items {
+		keys = append(keys, k)
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cache *Cache) PutTill(key, value string, deadline time.Time) {
+	cache.items[key] = Item{
+		Value:    value,
+		Deadline: deadline,
+		Expiring: true,
+	}
+}
+
+func (cache *Cache) cleanUp() {
+	for k, v := range cache.items {
+		if v.Expiring && v.Deadline.Before(time.Now()) {
+			delete(cache.items, k)
+		}
+	}
 }
